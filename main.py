5f9f7a966a8adec2fdcfcb78fb0e838e808810fd
cBombs <richie.ucsb@gmail.com>
2024-07-14 17:38:04 -0700

    start

---
+++ main.py
@@ -0,0 +1,23 @@
+# Pytorch Card Classifier
+# https://www.youtube.com/watch?v=tHL5STNJKag&t=290s&ab_channel=RobMulla
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+import matplotlib.pyplot as plt # For data viz
+import pandas as pd
+import numpy as np
+import sys
+from tqdm.notebook import tqdm
+
+print('System Version:', sys.version)
+print('PyTorch version', torch.__version__)
+print('Torchvision version', torchvision.__version__)
+print('Numpy version', np.__version__)
+print('Pandas version', pd.__version__)
